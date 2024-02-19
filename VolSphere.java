55bd4c3e51076b2e3e1fdb98461645c965c8aa2a
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-19 17:35:50 +0000

    will this fix the google format error?

--- VolSphere.java
+++ VolSphere.java
@@ -8,31 +8,31 @@ import java.util.Scanner;
  */
 
 public final class VolSphere {
-    /**
-     * Private constructor to prevent instantiation of this utility class.
-     */
-    private VolSphere() {
-    }
+  /**
+   * Private constructor to prevent instantiation of this utility class.
+   */
+  private VolSphere() {
+  }
 
-    /**
-     * The main method calculates volume of a sphere.
-     *
-     * @param args Command-line arguments (not used in this program)
-     */
-    public static void main(final String[] args) {
-        // import scanner and getting user radius from user
-        final Scanner sc = new Scanner(System.in);
-        System.out.println("Enter radius (cm): ");
-        final double radius = sc.nextDouble();
+  /**
+   * The main method calculates volume of a sphere.
+   *
+   * @param args Command-line arguments (not used in this program)
+   */
+  public static void main(final String[] args) {
+    // import scanner and getting user radius from user
+    final Scanner sc = new Scanner(System.in);
+    System.out.println("Enter radius (cm): ");
+    final double radius = sc.nextDouble();
 
-        // volume calculations
-        final double volume = (4d / 3d) * Math.PI * Math.pow(radius, 3);
+    // volume calculations
+    final double volume = (4d / 3d) * Math.PI * Math.pow(radius, 3);
 
-        // prints volume to console
-        System.out.format("The volume of the sphere is %.3f cm", volume);
-        System.out.println();
+    // prints volume to console
+    System.out.format("The volume of the sphere is %.3f cm", volume);
+    System.out.println();
 
-        // close scanner
-        sc.close();
-    }
+    // close scanner
+    sc.close();
+  }
 }
